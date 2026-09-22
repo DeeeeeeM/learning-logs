923f5582104e79ebbde579799276d936b7104a5f
heidel.parreno <parreno0718@gmanmi.com>
2026-09-22 19:50:49 +0800

    Finished chapter 1

--- backendDevelopment/HeadFirstJava/chapter1.java
+++ backendDevelopment/HeadFirstJava/chapter1.java
@@ -2,7 +2,31 @@ package backendDevelopment.HeadFirstJava;
 
 public class chapter1 {
     public static void main(String[] args) {
-        System.out.println("I Rule!");
-        System.out.println("The World!");
+        
+        String[] wordListOne = {"agnostic", "opinionated",
+        "voice activated", "haptically driven", "extensible",
+        "reactive", "agent based", "functional", "AI enabled",
+        "strongly typed"};
+
+        String[] wordListTwo = {"loosely coupled", "six sigma",
+        "asynchronous", "event driven", "pub-sub", "IoT", "cloud native", 
+        "service oriented", "containerized", "serverless",
+        "microservices", "distributed ledger"};
+
+        String[] wordListThree = {"framework", "library",
+        "DSL", "REST API", "repository", "pipeline", "service mesh", 
+        "architecture", "perspective", "design",
+        "orientation"};
+        
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+        java.util.Random randomGenerator = new java.util.Random();
+        int rand1 = randomGenerator.nextInt(oneLength);
+        int rand2 = randomGenerator.nextInt(twoLength);
+        int rand3 = randomGenerator.nextInt(threeLength);
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        System.out.println("What we need is a " + phrase);
     }
 }
